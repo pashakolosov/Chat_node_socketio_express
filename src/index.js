b5c02436d3784cab6fa73481cf228a9fd1a4de49
pasha <pasha.colocov@gmail.com>
2020-07-12 17:52:13 +0300

    'finish'

--- src/index.js
+++ src/index.js
@@ -2,11 +2,35 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
+const path = require('path');
 
 const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, '/../views/index.html'));
 });
 
 server.listen(3000, () => console.log(`server has been started on ${port} port`));
+
+
+users = [];
+connections = [];
+
+io.sockets.on('connection', socket => {
+    connections.push(socket);
+    console.log('Успешное соеденение');
+
+    socket.on('disconnect', (data) => {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('Отключение');
+    });
+
+    socket.on('send mess', (data) => {
+        io.sockets.emit('add mess', {
+            mess: data.mess,
+            name: data.name,
+            className: data.className
+        })
+    })
+});
+
